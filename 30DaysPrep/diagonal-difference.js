ceb60900eb6b4c1e42f92cf567469db957e66787
matheusrgg <matheusrgg@gmail.com>
2025-01-09 18:47:32 -0300

    mais doidera ainda

---
+++ 30DaysPrep/diagonal-difference.js
@@ -0,0 +1,3 @@
+// https://www.hackerrank.com/challenges/one-month-preparation-kit-diagonal-difference/problem?isFullScreen=true&h_l=interview&playlist_slugs%5B%5D=preparation-kits&playlist_slugs%5B%5D=one-month-preparation-kit&playlist_slugs%5B%5D=one-month-week-one
+
+
