e819334e6d2010fccb247fb3103a6a26ec81a52f
matheusrgg <matheusrgg@gmail.com>
2025-01-09 18:37:14 -0300

    q doideira

--- 30DaysPrep/sparse-arrays.js
+++ 30DaysPrep/sparse-arrays.js
@@ -1,22 +1,26 @@
+//https://www.hackerrank.com/challenges/one-month-preparation-kit-sparse-arrays/problem?isFullScreen=true&h_l=interview&playlist_slugs%5B%5D=preparation-kits&playlist_slugs%5B%5D=one-month-preparation-kit&playlist_slugs%5B%5D=one-month-week-one
+//https://www.youtube.com/watch?v=BZdnvTBO4vI
+//https://www.youtube.com/watch?v=n9w7Nuy-FmA 
+
+
+//HashMap Solution
+
+
 function matchingStrings(strings, queries) {
-    // Write your code here
-    let searchWord1 = 0
-    let searchWord2 = 0
-    let searchWord3 = 0
 
-    for( string of strings){
-
-        if(queries[0].includes(string)){
-            searchWord1 += 1
-        }else if(queries[1].includes(string)){
-            searchWord2 += 1
-        }else{
-            searchWord3 +=1
-        }
+    const memory = {};
+    for(const string of strings){
+        memory[string] = (memory[string] || 0 ) + 1;
     }
 
-    console.log(searchWord1, searchWord2, searchWord3);
-}
+    console.log("what", memory);
+    const arraySum = queries.map((query)=>{
+      return  memory[query] || 0
+    })
+
+    console.log(arraySum);
+    return arraySum
+}   
 
 
 
@@ -24,4 +28,28 @@ strings = ['aba', 'baba', 'aba', 'xzxb']
 queries = ['aba', 'xzxb', 'ab']
 matchingStrings(strings, queries)
 
+
+
+//minha tentativa
+
+
+// function matchingStrings(strings, queries) {
+//     // Write your code here
+//     let searchWord1 = 0
+//     let searchWord2 = 0
+//     let searchWord3 = 0
+
+//     for( string of strings){
+
+//         if(queries[0].includes(string)){
+//             searchWord1 += 1
+//         }else if(queries[1].includes(string)){
+//             searchWord2 += 1
+//         }else{
+//             searchWord3 +=1
+//         }
+//     }
+
+//     console.log(searchWord1, searchWord2, searchWord3);
+// }
 
